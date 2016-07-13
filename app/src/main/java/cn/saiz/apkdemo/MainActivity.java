ee91818a5a5bc9be852d1364a9e46680df7b8b3f
ZhangYouyi <zhangyouyi0@163.com>
2016-07-13 16:17:03 +0800

    清空扩展库

--- app/src/main/java/cn/saiz/apkdemo/MainActivity.java
+++ app/src/main/java/cn/saiz/apkdemo/MainActivity.java
@@ -5,17 +5,14 @@
 
 package cn.saiz.apkdemo;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 }
